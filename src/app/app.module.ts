3717b8ccf6df33c925a3e22b4ff7472e0c925109
Aurelijus Lasys <aurelijus.lasys@gmail.com>
2019-10-05 23:17:26 +0300

    Added a header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 
+import { MaterialModule } from './material.module';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,8 @@ import { HeaderComponent } from './header/header.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+
+import {
+    MatToolbarModule,
+} from '@angular/material';
+
+@NgModule({
+    imports: [
+      MatToolbarModule,
+    ],
+    exports: [
+      MatToolbarModule,
+    ],
+  })
+  export class MaterialModule { }
+  
+
