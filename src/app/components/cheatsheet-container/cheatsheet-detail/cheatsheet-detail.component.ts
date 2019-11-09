ffca1118822ead0b771483c0d19b7d52b764d5ed
Aurelijus Lasys <aurelijus.lasys@gmail.com>
2019-11-09 22:50:39 +0200

    Cheatsheet detail update

--- src/app/components/cheatsheet-container/cheatsheet-detail/cheatsheet-detail.component.ts
+++ src/app/components/cheatsheet-container/cheatsheet-detail/cheatsheet-detail.component.ts
@@ -13,4 +13,18 @@ export class CheatsheetDetailComponent implements OnInit {
     constructor() {}
 
     ngOnInit() {}
+
+    copyInstallCommand(textCopied: string): void {
+        let textCopiedElement = document.createElement('textarea');
+        textCopiedElement.style.position = 'fixed';
+        textCopiedElement.style.left = '0';
+        textCopiedElement.style.top = '0';
+        textCopiedElement.style.opacity = '0';
+        textCopiedElement.value = textCopied;
+        document.body.appendChild(textCopiedElement);
+        textCopiedElement.focus();
+        textCopiedElement.select();
+        document.execCommand('copy');
+        document.body.removeChild(textCopiedElement);
+    }
 }
